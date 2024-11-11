16ebc203a4a68824ff5696cddb268e01ca8a7b20
EduardoJr98 <153054937+EduardoJr98@users.noreply.github.com>
2024-11-11 00:46:57 -0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-Hi
+let nomeHeroi = "Thor";
+let xpHeroi = 4500;
+const mensagem = "O Herói de nome ";
+const mensagem2 = " está no nível de ";
+
+let nivel = "";
+
+if (xpHeroi <= 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpHeroi <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+console.log(mensagem + nomeHeroi + mensagem2 + nivel + ".");
